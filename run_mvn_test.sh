37d1d29e33c5f460906312fdc62e868044e4f50a
StarThinking <ma.1189@osu.edu>
2020-04-09 03:52:38 +0000

    fixed an error with grep

--- run_mvn_test.sh
+++ run_mvn_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ $# != 2 ]; then echo 'wrong: ./run_mvn_test.sh [project] [test_name]'; exit -1; fi
+if [ $# != 2 ]; then echo 'ERROR: ./run_mvn_test.sh [project] [test_name]'; exit -1; fi
 
 # yarn mapreduce hdfs hbase
 the_project=$1
 project_root_dir=$(cat /root/reconf_test_gen/"$the_project"/project_root_dir.txt)
-if [ $? -ne 0 ]; then echo 'error: project name is wrong'; exit -1; fi
+if [ $? -ne 0 ]; then echo 'ERROR: project name is wrong'; exit -1; fi
 
 the_test=$2
 classname=$(echo $the_test | awk -F '#' '{print $1}')
@@ -13,12 +13,12 @@ testname=$(echo $the_test | awk -F '#' '{print $2}')
 sub_project_classes_dir="/root/reconf_test_gen/"$the_project"/all_classes"
 
 suffix='all_classes.txt'
-sub_project="$project_root_dir""$(cd $sub_project_classes_dir; grep ^"$classname"^ *.txt | awk -F 'all_classes.txt' '{print $1}' | sed 's#%#/#g')"
-if [ "$sub_project" != "" ]; then
-    echo "sub_project for $the_test is $sub_project"
-else
-    echo "cannot find sub_project for $the_test"
+raw_sub_project=$(cd $sub_project_classes_dir; grep ^"$classname"$ *.txt | awk -F 'all_classes.txt' '{print $1}' | sed 's#%#/#g')
+if [ "$raw_sub_project" == "" ]; then
+    echo "ERROR: cannot find sub_project for $the_test"; exit -1;
 fi
+sub_project="$project_root_dir""$raw_sub_project"
+echo "sub_project for $the_test is $sub_project"
 
 # run mvn test
 #for sub in ${sub_projects[@]}
