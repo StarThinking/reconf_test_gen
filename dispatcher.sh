658d01baa5030dddc06375ca41a3f6e076e0b421
root <root@node-1.20-c220g5.postman-pg0.wisc.cloudlab.us>
2020-04-10 12:06:18 -0500

    new dispatcher

--- dispatcher.sh
+++ dispatcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-the_project='hbase'
+the_project='yarn'
 dst='/root/reconf_test_gen/'
 IFS=$'\n' 
 entry_list=( $(cat task.txt) )
@@ -8,12 +8,32 @@ entry_list_length=${#entry_list[@]}
 entry_cursor=0
 echo entry_list_length = $entry_list_length
 
+function is_idle {
+    i=$1
+    jps_num=$(docker exec hadoop-$i bash -c "jps" | wc -l)
+    sh_num=$(docker exec hadoop-$i bash -c "ps aux | grep run_mvn_test.sh" | wc -l)
+    if [ $jps_num -gt 1 ] || [ $sh_num -gt 2 ]; then
+	echo "no"
+    else
+	echo "yes"
+    fi
+}
+
 while [ $entry_cursor -lt $entry_list_length ]
 do
     for i in $(seq 0 19)
     do
-        jps_num=$(docker exec hadoop-$i bash -c "jps" | wc -l)
-        if [ $jps_num -gt 1 ]; then
+	idles[$i]=$(is_idle $i) & pids[$i]=$!
+    done
+
+    for i in $(seq 0 19)
+    do
+        wait ${pids[$i]}
+    done
+
+    for i in $(seq 0 19)
+    do
+        if [ "${ildes[$i]}" == "no" ]; then
 	     echo hadoop-$i is busy
 	else
 	    docker exec -d hadoop-$i bash -c "/root/reconf_test_gen/run_mvn_test.sh $the_project ${entry_list[$entry_cursor]} $dst"
