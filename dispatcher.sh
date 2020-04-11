4e7fbb856c2723487784c0139f54ba43ad5b179f
root <root@node-1.20-c220g5.postman-pg0.wisc.cloudlab.us>
2020-04-11 00:33:04 -0500

    update

--- dispatcher.sh
+++ dispatcher.sh
@@ -3,7 +3,7 @@
 if [ $# -ne 1 ]; then echo 'ERROR: ./dispatcher.sh [project]'; exit -1; fi
 
 the_project=$1
-vm_num=0 # 20
+vm_num=19 # 20
 busy_file=/tmp/reconf_busy
 dst='/root/reconf_test_gen/'
 IFS=$'\n' 
@@ -11,11 +11,12 @@ entry_list=( $(cat task.txt) )
 entry_list_length=${#entry_list[@]}
 entry_cursor=0
 echo entry_list_length = $entry_list_length
+cmd='echo /root/reconf_test_gen/run_mvn_test.sh $the_project ${entry_list[$entry_cursor]} $dst'
 
 function is_busy {
     i=$1
     busy=$(docker exec hadoop-$i bash -c "cat $busy_file")
-    if [ "$(busy)" != "true" ] && [ "$(busy)" != "false"]; then
+    if [ "$busy" != "true" ] && [ "$busy" != "false" ]; then
 	echo "ERROR: busy is wrong, $busy"; exit -1;
     else
 	echo "$busy"
@@ -25,7 +26,7 @@ function is_busy {
 # init busy file
 for i in $(seq 0 $vm_num)
 do
-    docker exec hadoop-$i bash -c "rm $busy_file; touch $busy_file; echo 'false' > $busy_file"
+    docker exec hadoop-$i bash -c "echo 'false' > $busy_file"
 done
 
 while [ $entry_cursor -lt $entry_list_length ]
@@ -35,8 +36,8 @@ do
         if [ "$(is_busy $i)" == "true" ]; then
 	     echo hadoop-$i is busy
 	else
-	    docker exec -d hadoop-$i bash -c "/root/reconf_test_gen/run_mvn_test.sh $the_project ${entry_list[$entry_cursor]} $dst"
 	    echo hadoop-$i is not busy, assign entry $entry_cursor to it
+	    docker exec -d hadoop-$i bash -c "$(eval $cmd)"
 	    entry_cursor=$(( entry_cursor + 1 ))
 	    if [ $entry_cursor -ge $entry_list_length ]; then echo finish all tasks; break; fi
 	fi
@@ -46,7 +47,7 @@ done
 
 for i in $(seq 0 $vm_num)
 do
-    while [ "$(is_busy $i)" == "false" ]; do sleep 10; done
+    while [ "$(is_busy $i)" == "true" ]; do sleep 10; done
 done
 
 echo all nodes are unbusy now
