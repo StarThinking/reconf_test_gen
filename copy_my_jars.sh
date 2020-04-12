da2fcad8ce41d292cf1a9c714d25fd127969ad6e
StarThinking <ma.1189@osu.edu>
2020-04-12 19:55:38 +0000

    update

--- copy_my_jars.sh
+++ copy_my_jars.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-cp /root/hadoop-3.2.1-src/hadoop-common-project/hadoop-common/target/hadoop-common-3.2.1.jar /root/.m2/repository/org/apache/hadoop/hadoop-common/3.2.1/
+cp /root/hadoop-3.2.1-src/hadoop-common-project/hadoop-common/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-common/3.2.1/
+ls -la /root/.m2/repository/org/apache/hadoop/hadoop-common/3.2.1/
 
 # HDFS
-cp /root/hadoop-3.2.1-src/hadoop-hdfs-project/hadoop-hdfs/target/hadoop-hdfs-3.2.1.jar /root/.m2/repository/org/apache/hadoop/hadoop-hdfs/3.2.1/
+cp /root/hadoop-3.2.1-src/hadoop-hdfs-project/hadoop-hdfs/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-hdfs/3.2.1/
+ls -la /root/.m2/repository/org/apache/hadoop/hadoop-hdfs/3.2.1/
 
 # Yarn
-cp /root/hadoop-3.2.1-src/hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-tests/target/hadoop-yarn-server-tests-3.2.1-tests.jar /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-tests/3.2.1/
+cp /root/hadoop-3.2.1-src/hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-tests/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-tests/3.2.1/
+ls -la /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-tests/3.2.1/
 
 # HBase HMaster HRegionServer
-cp /root/hbase-2.2.4/hbase-server/target/hbase-server-2.2.4.jar /root/.m2/repository/org/apache/hbase/hbase-server/2.2.4/
-cp /root/hbase-2.2.4/hbase-server/target/hbase-server-2.2.4-tests.jar /root/.m2/repository/org/apache/hbase/hbase-server/2.2.4/
+cp /root/hbase-2.2.4/hbase-server/target/*.jar /root/.m2/repository/org/apache/hbase/hbase-server/2.2.4/
+ls -la /root/.m2/repository/org/apache/hbase/hbase-server/2.2.4/
 
 # MapReduce
-cp /root/hadoop-3.2.1-src/hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient/target/hadoop-mapreduce-client-jobclient-3.2.1-tests.jar /root/.m2/repository/org/apache/hadoop/hadoop-mapreduce-client-jobclient/3.2.1/
+cp /root/hadoop-3.2.1-src/hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-mapreduce-client-jobclient/3.2.1/
+ls -la /root/.m2/repository/org/apache/hadoop/hadoop-mapreduce-client-jobclient/3.2.1/
