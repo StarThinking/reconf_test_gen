3d673d1a0a9300a5e05feb3158dc2423d13adac7
StarThinking <ma.1189@osu.edu>
2020-04-12 01:38:15 +0000

    add hdfs serevr jar

--- copy_my_jars.sh
+++ copy_my_jars.sh
@@ -2,6 +2,9 @@
 
 cp /root/hadoop-3.2.1-src/hadoop-common-project/hadoop-common/target/hadoop-common-3.2.1.jar /root/.m2/repository/org/apache/hadoop/hadoop-common/3.2.1/
 
+# HDFS
+cp /root/hadoop-3.2.1-src/hadoop-hdfs-project/hadoop-hdfs/target/hadoop-hdfs-3.2.1.jar /root/.m2/repository/org/apache/hadoop/hadoop-hdfs/3.2.1/
+
 # Yarn
 cp /root/hadoop-3.2.1-src/hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-tests/target/hadoop-yarn-server-tests-3.2.1-tests.jar /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-tests/3.2.1/
 
