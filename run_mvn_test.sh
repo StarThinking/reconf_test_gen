8409d1d664fa3be9d5fb9d13a067fab54cd40c5b
StarThinking <ma.1189@osu.edu>
2020-04-14 22:41:27 +0000

    update run_mvn_test.sh to support class#test

--- run_mvn_test.sh
+++ run_mvn_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $# -lt 2 ]; then echo 'ERROR: ./run_mvn_test.sh [project] [test_name] [optional: log_dts_dir]'; exit -1; fi
+if [ $# -lt 3 ]; then echo 'ERROR: ./run_mvn_test.sh [project] [test_name] [log_dts_dir|none]'; exit -1; fi
 
 # yarn mapreduce hdfs hbase
 the_project=$1
@@ -12,8 +12,7 @@ classname=$(echo $the_test | awk -F '#' '{print $1}')
 testname=$(echo $the_test | awk -F '#' '{print $2}')
 sub_project_classes_dir="/root/reconf_test_gen/"$the_project"/all_classes"
 
-log_dts_dir=''
-if [ $# -eq 3 ]; then log_dts_dir=$3; fi
+log_dts_dir="$3"
 
 suffix='all_classes.txt'
 raw_sub_project=$(cd $sub_project_classes_dir; grep ^"$classname"$ *.txt | awk -F 'all_classes.txt' '{print $1}' | sed 's#%#/#g')
@@ -25,11 +24,13 @@ sub_project="$project_root_dir""$raw_sub_project"
 
 # run mvn test
 #cd $sub_project; mvn test -Dtest=$the_test
+echo "the_test is $the_test"
+sleep 5
 cd $project_root_dir; mvn test -Dtest=$the_test
 
 # log
-test_log="$sub_project"/target/surefire-reports/"$the_test"-output.txt
+test_log="$sub_project"/target/surefire-reports/"$classname"-output.txt
 if [ ! -f $test_log ]; then echo 'ERROR: cannot find test_log for test $the_test'; exit -1; fi
 #echo "test_log is $test_log"
-if [ "$log_dts_dir" != "" ]; then mv $test_log $log_dts_dir; fi
+if [ "$log_dts_dir" != "none" ]; then mv $test_log $log_dts_dir; fi
 
