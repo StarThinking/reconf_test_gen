f2cccec1fda60f43e414786acad34e8d54273873
root <root@node-4.20-c220g5.postman-pg0.wisc.cloudlab.us>
2020-04-15 15:33:39 -0500

    update

--- run.sh
+++ run.sh
@@ -10,17 +10,17 @@ project_root_dir=$(cat /root/reconf_test_gen/"$the_project"/project_root_dir.txt
 if [ $? -ne 0 ]; then echo 'ERROR: wrong project name'; exit -1; fi
 
 # run mvn test
-#for sub in ${sub_projects[@]}
-#do
-#    echo $sub; cd $sub; mvn test > mvn_result.txt
-#done
-
-# get all class names
 for sub in ${sub_projects[@]}
 do
-    cd $sub; find . -name *output.txt | awk -F './target/surefire-reports/' '{print $2}' | awk -F '-output.txt' '{print $1}' | sort -u > all_classes.txt
+    echo $sub; cd $sub; mvn test > mvn_result.txt
 done
+
+# get all class names
+#for sub in ${sub_projects[@]}
+#do
+#    cd $sub; find . -name *output.txt | awk -F './target/surefire-reports/' '{print $2}' | awk -F '-output.txt' '{print $1}' | sort -u > all_classes.txt
+#done
     
-# cd into sub dir, save name-chaned all_classes.txt with sub project info
-cd $project_root_dir; for i in $(find . -name all_classes.txt); do copy_file=$(echo $i | sed 's#/#%#g' | sed 's/^..//'); echo $copy_file; mv $i $copy_file; done
-cd $project_root_dir; for i in $(find . -name mvn_result.txt); do copy_file=$(echo $i | sed 's#/#%#g' | sed 's/^..//'); echo $copy_file; mv $i $copy_file; done
+## cd into sub dir, save name-chaned all_classes.txt with sub project info
+#cd $project_root_dir; for i in $(find . -name all_classes.txt); do copy_file=$(echo $i | sed 's#/#%#g' | sed 's/^..//'); echo $copy_file; mv $i $copy_file; done
+#cd $project_root_dir; for i in $(find . -name mvn_result.txt); do copy_file=$(echo $i | sed 's#/#%#g' | sed 's/^..//'); echo $copy_file; mv $i $copy_file; done
