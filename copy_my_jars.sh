9f4583c2e7bb4c7ee6f1edbd7664e805f8b7909e
StarThinking <ma.1189@osu.edu>
2020-04-15 05:26:23 +0000

    hbase common for listener

--- copy_my_jars.sh
+++ copy_my_jars.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 cp /root/hadoop-3.2.1-src/hadoop-common-project/hadoop-common/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-common/3.2.1/
-ls -la /root/.m2/repository/org/apache/hadoop/hadoop-common/3.2.1/
+ls -la /root/.m2/repository/org/apache/hadoop/hadoop-common/3.2.1/ | grep jar
 
 # HDFS
 cp /root/hadoop-3.2.1-src/hadoop-hdfs-project/hadoop-hdfs/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-hdfs/3.2.1/
-ls -la /root/.m2/repository/org/apache/hadoop/hadoop-hdfs/3.2.1/
+ls -la /root/.m2/repository/org/apache/hadoop/hadoop-hdfs/3.2.1/ | grep jar
 
 # Yarn
 cp /root/hadoop-3.2.1-src/hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-tests/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-tests/3.2.1/
-ls -la /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-tests/3.2.1/
+ls -la /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-tests/3.2.1/ | grep jar
 
 # HBase HMaster HRegionServer
-cp /root/hadoop-3.2.1-src/hbase-2.2.4/hbase-server/target/*.jar /root/.m2/repository/org/apache/hbase/hbase-server/2.2.4/
-ls -la /root/.m2/repository/org/apache/hbase/hbase-server/2.2.4/
+cp /root/hbase-2.2.4/hbase-server/target/*.jar /root/.m2/repository/org/apache/hbase/hbase-server/2.2.4/
+cp /root/hbase-2.2.4/hbase-common/target/*.jar /root/.m2/repository/org/apache/hbase/hbase-common/2.2.4
+ls -la /root/.m2/repository/org/apache/hbase/hbase-server/2.2.4/ | grep jar
+ls -la /root/.m2/repository/org/apache/hbase/hbase-common/2.2.4/ | grep jar
 
 # MapReduce
 cp /root/hadoop-3.2.1-src/hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-mapreduce-client-jobclient/3.2.1/
-ls -la /root/.m2/repository/org/apache/hadoop/hadoop-mapreduce-client-jobclient/3.2.1/
+ls -la /root/.m2/repository/org/apache/hadoop/hadoop-mapreduce-client-jobclient/3.2.1/ | grep jar
