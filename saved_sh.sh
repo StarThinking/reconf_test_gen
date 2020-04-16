f40c347dd69693fc67b1127781590038878ca9be
root <root@node-4.20-c220g5.postman-pg0.wisc.cloudlab.us>
2020-04-16 18:19:13 -0500

    add mapreduce final

--- saved_sh.sh
+++ saved_sh.sh
@@ -1,3 +1,5 @@
 for i in org*; do if [ "$(grep msx $i | grep -v msx-listener)" == "" ]; then echo $i; fi; done | sort -u > ../no_init_classes.txt
 
 IFS=$'\n'; for i in $(cat all_tests.txt); do class=$(echo $i | awk -F '#' '{print $1}'); if [ "$(grep ^"$class"$ ./no_init_classes.txt)" == "" ] ; then echo $i; fi; done > raw_init_tests.txt
+
+for i in *; do cat $i | awk -F 'msx-' '{print msx $2}' > ../final/$i; done
