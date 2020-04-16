7c6968a1d3a9cfc6f92e60da91d8e756c6061050
root <root@node-4.20-c220g5.postman-pg0.wisc.cloudlab.us>
2020-04-15 20:41:42 -0500

    no init for hadoop-common

--- saved_sh.sh
+++ saved_sh.sh
@@ -1,3 +1,3 @@
-for i in org*; do if [ "$(grep msx $i | grep -v msx-listener)" != "" ]; then echo $i; fi; done
+for i in org*; do if [ "$(grep msx $i | grep -v msx-listener)" == "" ]; then echo $i; fi; done | sort -u > ../no_init_classes.txt
 
 IFS=$'\n'; for i in $(cat all_tests.txt); do class=$(echo $i | awk -F '#' '{print $1}'); if [ "$(grep ^"$class"$ ./no_init_classes.txt)" == "" ] ; then echo $i; fi; done > raw_init_tests.txt
