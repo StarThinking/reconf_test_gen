7edf32864deff5393d71a69268875dcf70faddbe
StarThinking <ma.1189@osu.edu>
2020-04-17 04:50:01 +0000

    hadoop 2.8.5 patch

--- copy_my_jars.sh
+++ copy_my_jars.sh
@@ -4,7 +4,6 @@
 # Hadoop Common
 ## Configuration (Optional)
 cp /root/reconf_test_gen/lib/hadoop-common-3.2.1.jar /root/.m2/repository/org/apache/hadoop/hadoop-common/3.2.1/
-echo ''
 
 ## Listener
 cp /root/hadoop-3.2.1-src/hadoop-common-project/hadoop-common/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-common/3.2.1/
@@ -28,6 +27,7 @@ echo ''
 cp /root/hadoop-3.2.1-src/hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-mapreduce-client-jobclient/3.2.1/
 
 ls -la /root/.m2/repository/org/apache/hadoop/hadoop-mapreduce-client-jobclient/3.2.1/ | grep "jar"$
+echo ''
 
 
 # HBase
@@ -43,8 +43,21 @@ cp /root/hbase-2.2.4/hbase-common/target/*.jar /root/.m2/repository/org/apache/h
 ls -la /root/.m2/repository/org/apache/hbase/hbase-common/2.2.4/ | grep "jar"$
 echo ''
 
-# Server
+## HBase Server
 cp /root/hbase-2.2.4/hbase-server/target/*.jar /root/.m2/repository/org/apache/hbase/hbase-server/2.2.4/
 
 ls -la /root/.m2/repository/org/apache/hbase/hbase-server/2.2.4/ | grep "jar"$
+echo ''
+
+## HDFS Yarn MapReduce
+cp /root/hadoop-2.8.5-src/hadoop-hdfs-project/hadoop-hdfs/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-hdfs/2.8.5/
+ls -la /root/.m2/repository/org/apache/hadoop/hadoop-hdfs/2.8.5/ | grep "jar"$
+echo ''
+
+cp /root/hadoop-2.8.5-src/hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-tests/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-tests/2.8.5/
+ls -la /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-tests/2.8.5/ | grep "jar"$
+echo ''
+
+cp /root/hadoop-2.8.5-src/hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-mapreduce-client-jobclient/2.8.5/
+ls -la /root/.m2/repository/org/apache/hadoop/hadoop-mapreduce-client-jobclient/2.8.5/ | grep "jar"$
 echo ''
