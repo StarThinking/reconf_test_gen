5eed2510909854741579e33da61d67dd5d7a6aed
root <root@node-0.20-c220g5.postman-pg0.wisc.cloudlab.us>
2020-04-16 23:13:38 -0500

    update

--- copy_my_jars.sh
+++ copy_my_jars.sh
@@ -2,39 +2,49 @@
 
 
 # Hadoop Common
+## Configuration (Optional)
+cp /root/reconf_test_gen/lib/hadoop-common-3.2.1.jar /root/.m2/repository/org/apache/hadoop/hadoop-common/3.2.1/
+echo ''
 
 ## Listener
 cp /root/hadoop-3.2.1-src/hadoop-common-project/hadoop-common/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-common/3.2.1/
 
-## Configuration (Optional) hadoop-common-2.8.5.jar is used by HBase-2.2.4
-cp /root/reconf_test_gen/lib/hadoop-common-2.8.5.jar /root/.m2/repository/org/apache/hadoop/hadoop-common/2.8.5/
-cp /root/reconf_test_gen/lib/hadoop-common-3.2.1.jar /root/.m2/repository/org/apache/hadoop/hadoop-common/3.2.1/
-
 ls -la /root/.m2/repository/org/apache/hadoop/hadoop-common/3.2.1/ | grep "jar"$
-echo ''
-ls -la /root/.m2/repository/org/apache/hadoop/hadoop-common/2.8.5/ | grep "jar"$
 echo ''
 
 # HDFS
 cp /root/hadoop-3.2.1-src/hadoop-hdfs-project/hadoop-hdfs/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-hdfs/3.2.1/
+
 ls -la /root/.m2/repository/org/apache/hadoop/hadoop-hdfs/3.2.1/ | grep "jar"$
 echo ''
 
 # Yarn
 cp /root/hadoop-3.2.1-src/hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-tests/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-tests/3.2.1/
+
 ls -la /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-tests/3.2.1/ | grep "jar"$
-echo ''
-
-# HBase
-cp /root/hbase-2.2.4/hbase-server/target/*.jar /root/.m2/repository/org/apache/hbase/hbase-server/2.2.4/
-ls -la /root/.m2/repository/org/apache/hbase/hbase-server/2.2.4/ | grep "jar"$
-echo ''
-
-## Listener
-cp /root/hbase-2.2.4/hbase-common/target/*.jar /root/.m2/repository/org/apache/hbase/hbase-common/2.2.4
-ls -la /root/.m2/repository/org/apache/hbase/hbase-common/2.2.4/ | grep "jar"$
 echo ''
 
 # MapReduce
 cp /root/hadoop-3.2.1-src/hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-mapreduce-client-jobclient/3.2.1/
+
 ls -la /root/.m2/repository/org/apache/hadoop/hadoop-mapreduce-client-jobclient/3.2.1/ | grep "jar"$
+
+
+# HBase
+## Configuration (Optional)
+cp /root/reconf_test_gen/lib/hadoop-common-2.8.5.jar /root/.m2/repository/org/apache/hadoop/hadoop-common/2.8.5/
+
+ls -la /root/.m2/repository/org/apache/hadoop/hadoop-common/2.8.5/ | grep "jar"$
+echo ''
+
+## HBaseListener
+cp /root/hbase-2.2.4/hbase-common/target/*.jar /root/.m2/repository/org/apache/hbase/hbase-common/2.2.4
+
+ls -la /root/.m2/repository/org/apache/hbase/hbase-common/2.2.4/ | grep "jar"$
+echo ''
+
+# Server
+cp /root/hbase-2.2.4/hbase-server/target/*.jar /root/.m2/repository/org/apache/hbase/hbase-server/2.2.4/
+
+ls -la /root/.m2/repository/org/apache/hbase/hbase-server/2.2.4/ | grep "jar"$
+echo ''
--- saved_sh.sh
+++ saved_sh.sh
@@ -3,3 +3,5 @@ for i in org*; do if [ "$(grep msx $i | grep -v msx-listener)" == "" ]; then ech
 IFS=$'\n'; for i in $(cat all_tests.txt); do class=$(echo $i | awk -F '#' '{print $1}'); if [ "$(grep ^"$class"$ ./no_init_classes.txt)" == "" ] ; then echo $i; fi; done > raw_init_tests.txt
 
 for i in *; do cat $i | awk -F 'msx-' '{print msx $2}' > ../final/$i; done
+
+for i in $(seq 0 19); do docker container stop hadoop-$i; docker container rm hadoop-$i; done; for i in $(seq 0 19); do docker run -d -it --name hadoop-$i sixiangma/reconf_parameter:v0.9.conf ; done; rm nohup.out; ./clean_update_all_container.sh 19; rm task.txt; git pull
