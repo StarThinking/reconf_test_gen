0e2f6d3332e561d04a02790e935b0029ad9b70a5
root <root@node-7.20-c220g5.postman-pg0.wisc.cloudlab.us>
2020-04-19 00:49:02 -0500

    update compress.sh

--- compress.sh
+++ compress.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 log=$1
+
+if [ "$(grep msx $log | grep -v msx-listener | grep -v msx-conf )" == "" ]; then echo 'no init'; exit 0; fi
+
 prefix=$(echo $log | awk -F '-output.txt' '{print $1}')
 grep msx $log | grep -v msx-conf > "$prefix"-component-meta.txt
 grep msx $log | grep msx-conf | sort -u > "$prefix"-parameter-meta.txt
