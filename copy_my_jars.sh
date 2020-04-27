c173223c0fe192f2f1c57626f735d7816b83d7fd
StarThinking <ma.1189@osu.edu>
2020-04-27 05:46:26 +0000

    patch for hadoop-3.2.1

--- copy_my_jars.sh
+++ copy_my_jars.sh
@@ -1,63 +1,61 @@
 #!/bin/bash
 
+function copy_jar_for_hadoop {
+    hadoop_version=$1
+    # Hadoop Common, Default Configuration and Reconf Agent
+    cp /root/hadoop-"$hadoop_version"-src/hadoop-common-project/hadoop-common/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-common/"$hadoop_version"/
+    ls -la /root/.m2/repository/org/apache/hadoop/hadoop-common/"$hadoop_version"/ | grep "jar"$
+    echo ''
+    
+    ## Configuration Override (Optional)
+    #cp /root/reconf_test_gen/lib/hadoop-common-"$hadoop_version".jar /root/.m2/repository/org/apache/hadoop/hadoop-common/"$hadoop_version"/
+    
+    # HDFS
+    cp /root/hadoop-"$hadoop_version"-src/hadoop-hdfs-project/hadoop-hdfs/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-hdfs/"$hadoop_version"/
+    ls -la /root/.m2/repository/org/apache/hadoop/hadoop-hdfs/"$hadoop_version"/ | grep "jar"$
+    echo ''
+    
+    # Yarn
+    ## MiniYARNCluster
+    cp /root/hadoop-"$hadoop_version"-src/hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-tests/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-tests/"$hadoop_version"/
+    ls -la /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-tests/"$hadoop_version"/ | grep "jar"$
+    echo ''
 
-# Hadoop Common
-## Default Configuration and Listener
-cp /root/hadoop-3.2.1-src/hadoop-common-project/hadoop-common/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-common/3.2.1/
-ls -la /root/.m2/repository/org/apache/hadoop/hadoop-common/3.2.1/ | grep "jar"$
-echo ''
+    ## nodemanager
+    cp /root/hadoop-"$hadoop_version"-src/hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-nodemanager/"$hadoop_version"/
+    ls -la /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-nodemanager/"$hadoop_version"/ | grep "jar"$
+    echo ''
+    
+    ## resourcemanager
+    cp /root/hadoop-"$hadoop_version"-src/hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-resourcemanager/"$hadoop_version"/
+    ls -la /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-resourcemanager/"$hadoop_version"/ | grep "jar"$
+    echo ''
+    
+    ## applicationhistoryservice
+    cp /root/hadoop-"$hadoop_version"-src/hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-applicationhistoryservice/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-applicationhistoryservice/"$hadoop_version"/
+    ls -la /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-applicationhistoryservice/"$hadoop_version"/ | grep "jar"$
+    echo ''
+    
+    # MapReduce
+    cp /root/hadoop-"$hadoop_version"-src/hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-mapreduce-client-jobclient/"$hadoop_version"/
+    ls -la /root/.m2/repository/org/apache/hadoop/hadoop-mapreduce-client-jobclient/"$hadoop_version"/ | grep "jar"$
+    echo ''
+}
 
-## Configuration Override (Optional)
-#cp /root/reconf_test_gen/lib/hadoop-common-3.2.1.jar /root/.m2/repository/org/apache/hadoop/hadoop-common/3.2.1/
+function copy_jar_for_hbase {
+    # HBase 2.2.4
+    ## Hbase Common, HBaseListener
+    cp /root/hbase-2.2.4/hbase-common/target/*.jar /root/.m2/repository/org/apache/hbase/hbase-common/2.2.4
+    ls -la /root/.m2/repository/org/apache/hbase/hbase-common/2.2.4/ | grep "jar"$
+    echo ''
+    
+    ## HBase Server
+    cp /root/hbase-2.2.4/hbase-server/target/*.jar /root/.m2/repository/org/apache/hbase/hbase-server/2.2.4/
+    ls -la /root/.m2/repository/org/apache/hbase/hbase-server/2.2.4/ | grep "jar"$
+    echo ''
+} 
 
-# HDFS
-cp /root/hadoop-3.2.1-src/hadoop-hdfs-project/hadoop-hdfs/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-hdfs/3.2.1/
-ls -la /root/.m2/repository/org/apache/hadoop/hadoop-hdfs/3.2.1/ | grep "jar"$
-echo ''
+copy_jar_for_hadoop "3.2.1"
+copy_jar_for_hadoop "2.8.5"
+copy_jar_for_hbase
 
-# Yarn
-cp /root/hadoop-3.2.1-src/hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-tests/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-tests/3.2.1/
-ls -la /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-tests/3.2.1/ | grep "jar"$
-echo ''
-
-# MapReduce
-cp /root/hadoop-3.2.1-src/hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-mapreduce-client-jobclient/3.2.1/
-ls -la /root/.m2/repository/org/apache/hadoop/hadoop-mapreduce-client-jobclient/3.2.1/ | grep "jar"$
-echo ''
-
-
-# HBase
-
-# Default Configuration
-# let maven download it
-rm /root/.m2/repository/org/apache/hadoop/hadoop-common/2.8.5/*.jar
-#cp /root/hadoop-2.8.5-src/hadoop-common-project/hadoop-common/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-common/2.8.5/
-#ls -la /root/.m2/repository/org/apache/hadoop/hadoop-common/2.8.5/ | grep "jar"$
-#echo ''
-## Configuration Override (Optional)
-#cp /root/reconf_test_gen/lib/hadoop-common-2.8.5.jar /root/.m2/repository/org/apache/hadoop/hadoop-common/2.8.5/
-#ls -la /root/.m2/repository/org/apache/hadoop/hadoop-common/2.8.5/ | grep "jar"$
-#echo ''
-
-## HBaseListener
-cp /root/hbase-2.2.4/hbase-common/target/*.jar /root/.m2/repository/org/apache/hbase/hbase-common/2.2.4
-ls -la /root/.m2/repository/org/apache/hbase/hbase-common/2.2.4/ | grep "jar"$
-echo ''
-
-## HBase Server
-cp /root/hbase-2.2.4/hbase-server/target/*.jar /root/.m2/repository/org/apache/hbase/hbase-server/2.2.4/
-ls -la /root/.m2/repository/org/apache/hbase/hbase-server/2.2.4/ | grep "jar"$
-echo ''
-
-## HDFS Yarn MapReduce
-cp /root/hadoop-2.8.5-src/hadoop-hdfs-project/hadoop-hdfs/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-hdfs/2.8.5/
-ls -la /root/.m2/repository/org/apache/hadoop/hadoop-hdfs/2.8.5/ | grep "jar"$
-echo ''
-
-cp /root/hadoop-2.8.5-src/hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-tests/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-tests/2.8.5/
-ls -la /root/.m2/repository/org/apache/hadoop/hadoop-yarn-server-tests/2.8.5/ | grep "jar"$
-echo ''
-
-cp /root/hadoop-2.8.5-src/hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-mapreduce-client-jobclient/2.8.5/
-ls -la /root/.m2/repository/org/apache/hadoop/hadoop-mapreduce-client-jobclient/2.8.5/ | grep "jar"$
-echo ''
