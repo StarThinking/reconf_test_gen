94c1c4a7e9060c6c6b978d35325a45640ffe895d
root <root@node-0.20c220g5-cluster.postman-pg0.wisc.cloudlab.us>
2020-04-29 14:49:37 -0500

    update

--- compress.sh
+++ compress.sh
@@ -3,7 +3,7 @@
 log=$1
 
 # we do not generate meta files if a test doesn't contain msx-reconfagent
-if [ "$(grep msx $log | grep -v msx-listener | grep -v msx-conf )" == "" ]; then echo 'no init'; exit 0; fi
+if [ "$(cat $log | grep "msx-reconfagent" | grep -v "reconf_vvmode=none" | grep -v "creating")" == "" ]; then echo 'no init'; exit 0; fi
 
 prefix=$(echo $log | awk -F '-output.txt' '{print $1}')
 grep msx $log | grep -v msx-conf > "$prefix"-component-meta.txt
