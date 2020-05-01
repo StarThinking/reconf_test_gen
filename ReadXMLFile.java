0272039dd7d3d2cd56b7ca290675fc09a42533c2
StarThinking <ma.1189@osu.edu>
2020-05-01 04:54:40 +0000

    update

--- ReadXMLFile.java
+++ ReadXMLFile.java
@@ -37,8 +37,37 @@ public class ReadXMLFile {
         }
     }
     
+    public static void getParameterValue(String xmlPath) {
+        try {
+    	    File xmlFile = new File(xmlPath);
+    	    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+    	    DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+    	    Document doc = dBuilder.parse(xmlFile);
+    
+    	    doc.getDocumentElement().normalize();
+    	    //System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+    
+    	    NodeList nList = doc.getElementsByTagName(ELEMENT_NAME);
+    	    for (int i=0; i<nList.getLength(); i++) {
+    	        Node nNode = nList.item(i);
+    	        if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+    	    	    Element eElement = (Element) nNode;
+    	    	    System.out.print(eElement.getElementsByTagName("name").item(0).getTextContent());
+		    if (eElement.getElementsByTagName("value").item(0) != null) {
+			System.out.print(" " + eElement.getElementsByTagName("value").item(0).getTextContent());
+		    }
+    	    	    System.out.println("");
+    	        }
+    	    }
+    	    //System.out.println("number of " + ELEMENT_NAME + " is " + nList.getLength());
+        } catch (Exception e) {
+    	    e.printStackTrace();
+        }
+    }
+
     public static void main(String args[]) { 
 	String xmlPath = args[0];
-	getAllParameters(xmlPath);
+	//getAllParameters(xmlPath);
+	getParameterValue(xmlPath);
     }
 }
