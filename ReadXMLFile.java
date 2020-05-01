ff662a002a3b735806b0d21541a0d2769a547cb8
StarThinking <ma.1189@osu.edu>
2020-05-01 04:36:59 +0000

    system parameters from .xml

--- ReadXMLFile.java
+++ ReadXMLFile.java
@@ -8,33 +8,37 @@ import java.io.File;
 
 public class ReadXMLFile {
 
-  private static final String ELEMENT_NAME = "property";
-
-  public static void main(String args[]) {
-    try {
-	String xmlPath = args[0];
-	File xmlFile = new File(xmlPath);
-	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-	Document doc = dBuilder.parse(xmlFile);
-
-	doc.getDocumentElement().normalize();
-	//System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-
-	NodeList nList = doc.getElementsByTagName(ELEMENT_NAME);
-	for (int i=0; i<nList.getLength(); i++) {
-	    Node nNode = nList.item(i);
-	    if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-		Element eElement = (Element) nNode;
-		System.out.println("name : " + eElement.getElementsByTagName("name").item(0).getTextContent());
-		System.out.println("value : " + eElement.getElementsByTagName("value").item(0).getTextContent());
-		System.out.println("");
-	    }
-	}
-	System.out.println("number of " + ELEMENT_NAME + " is " + nList.getLength());
-    } catch (Exception e) {
-	e.printStackTrace();
+    private static final String ELEMENT_NAME = "property";
+  
+    public static void getAllParameters(String xmlPath) {
+        try {
+    	    File xmlFile = new File(xmlPath);
+    	    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+    	    DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+    	    Document doc = dBuilder.parse(xmlFile);
+    
+    	    doc.getDocumentElement().normalize();
+    	    //System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+    
+    	    NodeList nList = doc.getElementsByTagName(ELEMENT_NAME);
+    	    for (int i=0; i<nList.getLength(); i++) {
+    	        Node nNode = nList.item(i);
+    	        if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+    	    	    Element eElement = (Element) nNode;
+    	    	    //System.out.println("name : " + eElement.getElementsByTagName("name").item(0).getTextContent());
+    	    	    //System.out.println("value : " + eElement.getElementsByTagName("value").item(0).getTextContent());
+    	    	    //System.out.println("");
+		    System.out.println(eElement.getElementsByTagName("name").item(0).getTextContent());
+    	        }
+    	    }
+    	    //System.out.println("number of " + ELEMENT_NAME + " is " + nList.getLength());
+        } catch (Exception e) {
+    	    e.printStackTrace();
+        }
     }
-  }
-
+    
+    public static void main(String args[]) { 
+	String xmlPath = args[0];
+	getAllParameters(xmlPath);
+    }
 }
