b2794d3e22bdc21a44e9e2bae2960e8490d3b8d7
root <ma.1189@osu.edu>
2020-05-12 19:56:42 +0000

    update

--- javassist_study/thread/msx/thread/Multi.java
+++ javassist_study/thread/msx/thread/Multi.java
@@ -11,25 +11,37 @@ class Multi extends Thread {
             " Name is " + Thread.currentThread().getName());
     } 
 
+    private static void printStackTrace(StackTraceElement[] stack) {
+        if (stack != null) {
+            System.out.println("stack.length = " + stack.length);
+            for (int i = 1; i < stack.length; i++) {
+                StackTraceElement s = stack[i];
+                System.out.println("\tat " + s.getClassName() + "." + s.getMethodName() + 
+            	"(" + s.getFileName() + ":" + s.getLineNumber() + ")");
+            }
+        }
+    }
+
     public static void main(String args[]) throws Exception {  
-	Thread.setCreateContext("Parent");
         System.out.println("Parent Current Thread ID is " + Thread.currentThread().getId() + 
             " Name is " + Thread.currentThread().getName());
         Multi t1 = new Multi(); 
         t1.setName("Child Thread");
         System.out.println("Thread Relation: " + Thread.currentThread().getId() + " -> " + t1.getId());
         t1.start();
-	//Thread.setCreateContext("None");
 
-        Map<String, List<Long>> contextMap = Thread.getCreateContextMap();
-        for (String key : contextMap.keySet()) {
-	    System.out.println("context: " + key);
-            List<Long> list = contextMap.get(key);
-	    if (list != null) {
-		for (Long id : list) {
-		    System.out.println(id);
-		}
-	    }
+	System.out.println("startStackMap:");
+        Map<Long, StackTraceElement[]> startStackMap = Thread.getStartStackMap();
+        for (Long tid : startStackMap.keySet()) {
+	    System.out.println("tid = " + tid);
+	    printStackTrace(startStackMap.get(tid));
+        }
+	
+	System.out.println("initStackMap:");
+        Map<Long, StackTraceElement[]> initStackMap = Thread.getInitStackMap();
+        for (Long tid : initStackMap.keySet()) {
+	    System.out.println("tid = " + tid);
+	    printStackTrace(initStackMap.get(tid));
         }
     }  
 }  
