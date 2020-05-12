a44f653ac4d5c8f8b3f0de800b08c2b7ea08fb9f
StarThinking <ma.1189@osu.edu>
2020-05-12 03:22:38 +0000

    update

--- javassist_study/thread/msx/thread/Multi.java
+++ javassist_study/thread/msx/thread/Multi.java
@@ -1,6 +1,7 @@
 package msx.thread;
 
 import java.util.List;
+import java.util.Map;
 import java.util.ArrayList;
 
 class Multi extends Thread {  
@@ -11,16 +12,24 @@ class Multi extends Thread {
     } 
 
     public static void main(String args[]) throws Exception {  
+	Thread.setCreateContext("Parent");
         System.out.println("Parent Current Thread ID is " + Thread.currentThread().getId() + 
             " Name is " + Thread.currentThread().getName());
         Multi t1 = new Multi(); 
         t1.setName("Child Thread");
         System.out.println("Thread Relation: " + Thread.currentThread().getId() + " -> " + t1.getId());
         t1.start();
+	//Thread.setCreateContext("None");
 
-        List<String> createRelationList = Thread.getCreateRelationList();
-        for (String relation : createRelationList) {
-            System.out.println("relation: " + relation);
+        Map<String, List<Long>> contextMap = Thread.getCreateContextMap();
+        for (String key : contextMap.keySet()) {
+	    System.out.println("context: " + key);
+            List<Long> list = contextMap.get(key);
+	    if (list != null) {
+		for (Long id : list) {
+		    System.out.println(id);
+		}
+	    }
         }
     }  
 }  
