e92ed5e2e4cc4bb2d2e500ebff73cc11835c2fbb
StarThinking <ma.1189@osu.edu>
2020-05-13 04:26:07 +0000

    add something

--- compress.sh
+++ compress.sh
@@ -6,6 +6,10 @@ log=$1
 if [ "$(cat $log | grep "msx-reconfagent" | grep -v "reconf_vvmode=none" | grep -v "creating")" == "" ]; then echo 'no init'; exit 0; fi
 
 prefix=$(echo $log | awk -F '-output.txt' '{print $1}')
-grep msx $log | grep -v msx-stack | grep -v msx-conf > "$prefix"-component-meta.txt
-grep msx $log | grep msx-conf | sort -u > "$prefix"-parameter-meta.txt
-grep msx $log | grep msx-stack > "$prefix"-stack-meta.txt
+grep msx $log | grep -v 'msx-conf' > "$prefix"-component-meta.txt
+
+rm "$prefix"-parameter-meta.txt.tmp 2> /dev/null
+grep msx $log | grep 'msx-conf' | sort -u > "$prefix"-parameter-meta.txt.tmp
+cat "$prefix"-parameter-meta.txt.tmp | awk -F 'msx-conf' '{print "msx-conf"$2}' > "$prefix"-parameter-meta.txt
+rm "$prefix"-parameter-meta.txt.tmp
+
