e3124fbe6bac50ef8ced771c0bc3793328c766a4
StarThinking <ma.1189@osu.edu>
2020-05-15 14:34:31 +0000

    add a yarn stack_based_mapping sample

--- stack_based_mapping/gen_component_para.sh
+++ stack_based_mapping/gen_component_para.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 if [ $# -ne 1 ]; then echo 'wrong'; exit -1; fi
 
-grep NameNode $1 | sort -u | awk '{print $2}' > namenode.txt
-grep DataNode $1 | sort -u | awk '{print $2}' > datanode.txt
-grep JournalNode $1 | sort -u | awk '{print $2}' > journalnode.txt
-grep Other $1 | sort -u | awk '{print $2}' > other.txt
+grep NameNode $1 | sort -u | awk '{print $2}' > NameNode.txt
+grep DataNode $1 | sort -u | awk '{print $2}' > DataNode.txt
+grep JournalNode $1 | sort -u | awk '{print $2}' > JournalNode.txt
+grep ApplicationHistoryServer $1 | sort -u | awk '{print $2}' > ApplicationHistoryServer.txt
+grep NodeManager $1 | sort -u | awk '{print $2}' > NodeManager.txt
+grep ResourceManager $1 | sort -u | awk '{print $2}' > ResourceManager.txt
+grep Other $1 | sort -u | awk '{print $2}' > Other.txt
