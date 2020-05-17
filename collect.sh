b0f013a5d6bbdffaeba6e5414610899d86362c62
root <root@node-0.msx-18.postman-pg0.wisc.cloudlab.us>
2020-05-17 00:22:29 -0500

    yarn new final with stack parameter

--- collect.sh
+++ collect.sh
@@ -11,18 +11,18 @@ do
     wait $p; echo "$p finished tranferring"
 done
 
-#for i in $(grep -oP "node-[0-9]{1,2}$" /etc/hosts | sed 's/node-//g' | sort -n)
-#do 
-#    ssh node-$i 'cd ~/reconf_test_gen/target/; for l in *; do ~/reconf_test_gen/compress.sh $l; done;' &
-#    pids1[$i]=$!
-#done
-#
-#for p in ${pids1[@]}
-#do
-#    wait $p; echo "$p finished compression"
-#done
-#    
+for i in $(grep -oP "node-[0-9]{1,2}$" /etc/hosts | sed 's/node-//g' | sort -n)
+do 
+    ssh node-$i 'cd ~/reconf_test_gen/target/; for l in *; do ~/reconf_test_gen/compress.sh $l; done; rm *-output.txt' &
+    pids1[$i]=$!
+done
+
+for p in ${pids1[@]}
+do
+    wait $p; echo "$p finished compression"
+done
+    
 for i in $(grep -oP "node-[0-9]{1,2}$" /etc/hosts | sed 's/node-//g' | sort -n)
 do
-    scp node-$i:~/reconf_test_gen/target/* .
+    scp node-$i:~/reconf_test_gen/target/* ~/reconf_test_gen/target/
 done
