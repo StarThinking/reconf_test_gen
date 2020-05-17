c86d54b70889877c82095b860d1aed4fac9544b2
root <root@node-0.fzhou-qv71583.osu-nfs-test-pg0.wisc.cloudlab.us>
2020-05-17 00:57:33 -0500

    update

--- collect.sh
+++ collect.sh
@@ -1,28 +1,19 @@
 #!/bin/bash
 
-for i in $(grep -oP "node-[0-9]{1,2}$" /etc/hosts | sed 's/node-//g' | sort -n)
+#for i in $(grep -oP "node-[0-9]{1,2}$" /etc/hosts | sed 's/node-//g' | sort -n)
+for i in $(seq 1 17)
 do 
-    ssh node-$i 'rm ~/reconf_test_gen/target/*; ~/parameter_test_controller/container_utility_sh/docker_fetch_result.sh .txt ~/reconf_test_gen/target/ ~/reconf_test_gen/target/' &
+    ssh node-$i 'rm ~/reconf_test_gen/target/*; ~/parameter_test_controller/container_utility_sh/docker_fetch_compress_result.sh .txt ~/reconf_test_gen/target/ ~/reconf_test_gen/target/' &
     pids0[$i]=$!
 done
 
 for p in ${pids0[@]}
 do
-    wait $p; echo "$p finished tranferring"
+    wait $p; echo "$p finished compressing and tranferring"
 done
-
+    
 #for i in $(grep -oP "node-[0-9]{1,2}$" /etc/hosts | sed 's/node-//g' | sort -n)
-#do 
-#    ssh node-$i 'cd ~/reconf_test_gen/target/; for l in *; do ~/reconf_test_gen/compress.sh $l; done;' &
-#    pids1[$i]=$!
-#done
-#
-#for p in ${pids1[@]}
-#do
-#    wait $p; echo "$p finished compression"
-#done
-#    
-for i in $(grep -oP "node-[0-9]{1,2}$" /etc/hosts | sed 's/node-//g' | sort -n)
+for i in $(seq 1 17)
 do
     scp node-$i:~/reconf_test_gen/target/* .
 done
