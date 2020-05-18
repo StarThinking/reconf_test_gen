0f5e6dfd9f5780c19a45dfdeb7ab1089b7cfeae9
StarThinking <ma.1189@osu.edu>
2020-05-18 22:32:07 +0000

    update

--- generate_ultimate_meta_for_test.sh
+++ generate_ultimate_meta_for_test.sh
@@ -10,6 +10,8 @@ test_name=$(echo $output_log | awk -F '-output.txt' '{print $1}')
 component_log="$dir"/"$test_name"-component-meta.txt
 parameter_log="$dir"/"$test_name"-parameter-meta.txt
 
+if [ ! -f $component_log ] || [ ! -f $parameter_log ]; then echo 'no component_log or parameter_log'; exit 0; fi
+
 num=$(cat "$component_log" | grep 'conf with hashCode' | wc -l)
 component_conf_hc_array=( $(cat "$component_log" | grep 'conf with hashCode' | awk '{print $(NF-2)}') )
 component_name_array=( $(cat "$component_log" | grep 'conf with hashCode' | awk '{print $NF}') )
@@ -19,7 +21,7 @@ component_name_array=( $(cat "$component_log" | grep 'conf with hashCode' | awk 
 #    echo "name: ${component_name_array[$i]}"
 #done
 
-function get_component_name_by_conf_hc {
+function get_component_name_by_conf_hc {    
     hc=$1
     index=-1
     for i in $(seq 0 $(( num - 1 )))
@@ -32,7 +34,7 @@ function get_component_name_by_conf_hc {
     if [ $index -eq -1 ] ;then
 	echo "OtherComponent"
     else
-        echo "${component_name_array[$index]}"
+        echo "${component_name_array[$index]}"."$index"
     fi
 }
 
@@ -51,4 +53,4 @@ function main {
     done
 }
 
-main | sort -k1 -k2 -k3  > ./"$test_name"-ultimate-meta.txt
+main | sort -k1 -k2 -k3 | sort -u > ./"$test_name"-ultimate-meta.txt
