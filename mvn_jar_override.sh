7071612750830fbe29761adb21c7411af1bfb54c
StarThinking <ma.1189@osu.edu>
2020-06-04 03:30:56 +0000

    patch with parameter print

--- mvn_jar_override.sh
+++ mvn_jar_override.sh
@@ -25,10 +25,14 @@ function per_override_task {
 hadoop_version='3.2.1'
 echo "perform jar override for hadoop common $hadoop_version"
 cp /root/hadoop-"$hadoop_version"-src/hadoop-common-project/hadoop-common/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-common/"$hadoop_version"/
+# double override
+cp /root/reconf_test_gen/javassist_study/conf_return/hadoop-common-3.2.1.jar /root/.m2/repository/org/apache/hadoop/hadoop-common/"$hadoop_version"/
 
 hadoop_version='2.8.5'
 echo "perform jar override for hadoop common $hadoop_version"
 cp /root/hadoop-"$hadoop_version"-src/hadoop-common-project/hadoop-common/target/*.jar /root/.m2/repository/org/apache/hadoop/hadoop-common/"$hadoop_version"/
+# double override
+cp /root/reconf_test_gen/javassist_study/conf_return/hadoop-common-2.8.5.jar /root/.m2/repository/org/apache/hadoop/hadoop-common/"$hadoop_version"/
 
 # hbase common: Listener
 echo "perform jar override for hbase common 2.2.4"
