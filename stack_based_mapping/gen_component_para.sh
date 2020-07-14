44a48702fbf62e07eed69641273e76ff302d0042
StarThinking <ma.1189@osu.edu>
2020-07-14 01:13:51 +0000

    update

--- stack_based_mapping/gen_component_para.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ $# -ne 1 ]; then echo 'wrong'; exit -1; fi
-
-for component in NameNode DataNode JournalNode ApplicationHistoryServer NodeManager ResourceManager Other 
-do
-    grep ' is used by ' $1 | grep "$component"$ | awk '{print $2}' | sort -u > "$component".txt
-done
