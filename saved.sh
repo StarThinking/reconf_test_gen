fa282ec13902202cc4639c904d0b14c0f6cb013f
root <root@node-0.the-final.postman-pg0.wisc.cloudlab.us>
2020-07-26 17:04:14 -0500

    add all_parameters_xml.txt for each projects

--- saved.sh
+++ saved.sh
@@ -1,3 +1,8 @@
+# structure final
+for i in $(seq 0 15); do tar zxvf $i.tar.gz ; done; rm *.tar.gz
+mkdir component; mkdir parameter; mkdir ultimate; mv *-component-meta.txt component; mv *-parameter-meta.txt parameter; mv *-ultimate-meta.txt ultimate;
+mkdir final; mv * final
+
 # get hbase main+conf components
 grep -rn 'static void main(' | awk -F ':' '{print $1}' | grep .java | grep -v '/test/' | while read line; do if [ "$(grep 'HBaseConfiguration.create' $line)" != "" ]; then echo $line; fi; done
 
