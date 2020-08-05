6b9c5893255b58090052465358cf29db09952a2b
StarThinking <ma.1189@osu.edu>
2020-08-05 20:51:56 +0000

    update javassist hacked hadoop-common jars and compress.sh to genrate 3 .txt from 1 -output.txt

--- compress.sh
+++ compress.sh
@@ -3,13 +3,11 @@
 log=$1
 
 # we do not generate meta files if a test doesn't contain msx-reconfagent
-if [ "$(cat $log | grep "msx-reconfagent" | grep "performReconf")" == "" ]; then echo 'no init'; exit 0; fi
+#if [ "$(cat $log | grep "msx-reconfagent" | grep "performReconf")" == "" ]; then echo 'no init'; exit 0; fi
 
 prefix=$(echo $log | awk -F '-output.txt' '{print $1}')
-grep msx $log | grep -v 'msx-conf' > "$prefix"-component-meta.txt
+grep msx $log | grep -v 'msx-get' > "$prefix"-component-meta.txt
 
-rm "$prefix"-parameter-meta.txt.tmp 2> /dev/null
-grep msx $log | grep 'msx-conf' > "$prefix"-parameter-meta.txt.tmp
-cat "$prefix"-parameter-meta.txt.tmp | awk -F 'msx-conf' '{print "msx-conf"$2}' | sort -u > "$prefix"-parameter-meta.txt
-rm "$prefix"-parameter-meta.txt.tmp
+grep msx $log | grep 'msx-get' | awk -F 'msx-get ' '{print $2}' | sort -u > "$prefix"-parameter-meta.txt
 
+cat "$prefix"-parameter-meta.txt | awk '{print $1" "$2" "$3}' | sort -u > "$prefix"-ultimate-meta.txt
