be7059cdac79ab70f4bfeb744517bd90f50b4b08
StarThinking <ma.1189@osu.edu>
2020-08-06 04:11:29 +0000

    remove unnecessary sh

--- old_saved_sh.sh
+++ old_saved_sh.sh
@@ -1,17 +1,6 @@
 # run reconf_test dispatcher distributedly
 proj='hdfs'; for i in $(grep -oP "node-[0-9]{1,2}$" /etc/hosts | sed 's/node-//g' | sort -n); do ssh node-$i "rm ~/nohup.txt; ps aux | grep dispatcher | awk '{print $2}' | xargs kill -9; nohup ~/reconf_test_gen/dispatcher.sh $proj > nohup.txt &" & pids[$i]=$!; done; for p in ${pids[@]}; do wait $p; echo "$p is done"; done
 
-for i in $(grep -oP "node-[0-9]{1,2}$" /etc/hosts | sed 's/node-//g' | sort -n); do ssh node-$i "rm ~/reconf_test_gen/target/*.txt; ~/parameter_test_controller/container_utility_sh/docker_fetch_result.sh .txt ~/reconf_test_gen/target/ ~/reconf_test_gen/target/"; cd ~/reconf_test_gen/target/; for l in ./*; do ~/reconf_test_gen/compress.sh $l; done; scp node-$i:~/reconf_test_gen/target/*.txt .; done
-
-
-for i in org*; do if [ "$(cat $i | grep "msx-reconfagent" | grep -v "reconf_vvmode=none" | grep -v "creating")" == "" ]; then echo $i; fi; done | sort -u > ../no_init_classes.txt
-
-../extract_all_tests_from_dir.sh ./all_classes_hacked | sort -u
-
-IFS=$'\n'; for i in $(cat all_tests.txt); do class=$(echo $i | awk -F '#' '{print $1}'); if [ "$(grep ^"$class"$ ./no_init_classes.txt)" == "" ] ; then echo $i; fi; done > raw_init_tests.txt
-
-for i in $(seq 0 19); do docker container stop hadoop-$i; docker container rm hadoop-$i; done; for i in $(seq 0 19); do docker run -d -it --name hadoop-$i sixiangma/reconf_parameter:v1.9.conf ; done; rm nohup.out; ./clean_update_all_container.sh 19; rm task.txt; git pull
-
 # for classes
 ./docker_fetch_result.sh; for i in org*.txt; do grep msx $i | grep -v msx-conf > $(echo $i | awk -F '-output.txt' '{print $1}'); done; rm *-output.txt; ls | grep org | wc -l; cat task.txt | wc -l
 
@@ -31,5 +20,3 @@ find COMPONENT_SERVER_CLASS_PATH_PREFIX -name '*.java' | sed 's#/#.#g' | sed 's/
 
 # make up list add statement
 cat server_classes.txt | sed 's/^/classList.add("/g' | sed 's/$/");/g'
-
-
