360e4b5fbc7dc826cc29db954f4a087d6032b85f
StarThinking <ma.1189@osu.edu>
2020-08-22 04:36:43 +0000

    update jars

--- javassist_study/conf_return/saved.sh
+++ javassist_study/conf_return/saved.sh
@@ -1,3 +1,3 @@
 javac -cp .:../javassist/javassist.jar Convert.java
 
-java -cp .:../javassist/javassist.jar:./hadoop-common-3.2.1.jar Convert
+java -cp .:../javassist/javassist.jar:./hadoop-common-?.?.?.jar Convert
