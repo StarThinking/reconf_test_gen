7bf38ded64668ceaa0fa236bc3a3bc50d0d0c8dc
StarThinking <ma.1189@osu.edu>
2020-12-31 02:15:52 +0000

    update

--- nohup_test.sh
+++ nohup_test.sh
@@ -3,4 +3,4 @@
 file_name=$1
 proj=$2
 
-for i in $(cat $file_name); do ./run_mvn_test.sh $proj $i ~/reconf_test_gen/target/; done
+for i in $(cat $file_name); do /root/reconf_test_gen/run_mvn_test.sh $proj $i /root/reconf_test_gen/target/; done
